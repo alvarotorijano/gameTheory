ae36389570aff62dd1bb4cdbfc8afefbe29b56f6
Xu <jiehao.xu@accenture.com>
2026-09-09 12:32:51 +0200

    My startegy agent

---
+++ agents/my_strategy/agent.py
@@ -0,0 +1,38 @@
+"""
+Copycat Agent (Tit-for-Tat) for the Iterated Prisoner's Dilemma.
+
+Cooperates on round 1, then plays the opponent's previous move.
+This is a template for student agents to study and modify.
+"""
+
+from typing import List
+
+from utils.game_core import Agent, COOPERATE, DEFECT
+
+
+class CopycatAgent(Agent):
+    """
+    Tit-for-Tat strategy: cooperate on round 1, then copy opponent's last move.
+
+    This is one of the most famous strategies in game theory. It's proven to be
+    very effective because it's:
+    - Nice: starts with cooperation
+    - Retaliatory: punishes defection immediately
+    - Forgiving: returns to cooperation if opponent does
+
+    This agent is provided as a template for student assignments.
+    """
+
+    def play(self, own_history: List[str], opponent_history: List[str]) -> str:
+        """
+        Cooperate on round 1, then replicate opponent's last move.
+
+        Parameters:
+            own_history: This agent's past moves (unused in tit-for-tat).
+            opponent_history: Opponent's past moves.
+
+        Returns:
+            "C" (cooperate) on round 1, otherwise opponent's last move.
+        """
+        
+        return DEFECT
